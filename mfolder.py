354610f1ea63511ba342bacb356320155da51d91
npbcts <auditx@hotmail.com>
2022-10-24 14:05:15 +0800

    修改文件夹操作模块

--- mfolder.py
+++ mfolder.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import logging
+from typing import List
 
 
 def mk_folder(folder: str, rebuild=False) -> None:
@@ -33,3 +34,45 @@ def rm_folder(folder: str) -> None:
         logging.info(f'folder: {folder} deleted already!')
     else:
         logging.info(f'have no find {folder}, please check folder path!')
+
+
+def files_2_folder(files: List[str], source_folder: str, aim_folder: str) -> None:
+    """
+    files_2_folder: 将source_folder中的文件复制到aim_folder文件夹。
+    Args:
+        files (List[str]): 脚本文件名的列表
+        source_folder(str): 源文件夹, path like类型, 即文件夹路径
+        aim_folder(str): 目标文件夹, path like类型, 即文件夹路径
+    """
+    log_str = 'can`t find folder {}, please check!'
+    path_exist = True
+    if not os.path.exists(source_folder):
+        logging.info(log_str.format(source_folder))
+        path_exist = False
+    if not os.path.exists(aim_folder):
+        logging.info(log_str.format(aim_folder))
+        path_exist = False
+    if not path_exist:
+        return
+    for file in files:
+        source_file = os.path.join(source_folder, file)
+        if not os.path.exists(source_file):
+            logging.info(log_str.format(source_file))
+            continue  # 需要复制的源文件不存在，进入复制文件的下一个循环
+        shutil.copy(source_file, os.path.join(aim_folder, file))
+
+
+def del_folder_files(files: List[str], aim_folder) -> None:
+    """
+    del_folder_files: 删除目标文件夹中无用文件。
+
+    Args:
+    aim_folder: 待删除文件所在的文件夹
+        script_files (List[str]): 文件名的列表
+    """
+    for file in files:
+        file_path = os.path.join(aim_folder, file)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        else:
+            logging.info(f"file {file_path} does not exist")
