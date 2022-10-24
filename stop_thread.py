916e62d7e10148b8eeded33f21ffc014a7e2b6a4
npbcts <auditx@hotmail.com>
2022-10-24 23:57:54 +0800

    增加stop_thread模块

---
+++ stop_thread.py
@@ -0,0 +1,26 @@
+import threading
+import logging
+import ctypes
+
+
+def terminate_thread(thread: threading.Thread):
+    """Terminates a python thread from another thread.
+
+    :param thread: a threading.Thread instance
+
+    from: https://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread
+    """
+    if not thread.is_alive():
+        return
+
+    exc = ctypes.py_object(SystemExit)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
+        ctypes.c_long(thread.ident), exc)
+    if res == 0:
+        raise ValueError("nonexistent thread id")
+    elif res > 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(thread.ident, None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+    logging.info(f'{thread} is terminate')
