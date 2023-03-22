34bf1e66dad23c37eac8b1581469c322f08287ce
npbcts <auditx@hotmail.com>
2023-03-22 17:08:13 +0800

    为日志记录器添加自定义保存路径

--- mylogger.py
+++ mylogger.py
@@ -3,23 +3,33 @@ import logging
 import logging.handlers
 
 
-this_folder = os.path.dirname(os.path.abspath(__file__))
-log_folder = os.path.join(this_folder, "logs")
-if os.path.exists(log_folder) is False:
-    os.mkdir(log_folder)
-
 __all__ = ["get_logger"]
 
 
-def get_logger(filename: str) -> logging.Logger:
+def get_logger(filename: str, log_save_folder: str) -> logging.Logger:
     """
-    get_logger 输入文件名获取一个日志记录器
+    get_logger 输入文件名和日志记录文件路径(在此路径下建立logs文件夹),获取一个日志记录器\n
+        日志文件全部保存为: log_save_folder/logs/xxx.log
+
     Args:
-        filename (str): _description_
+        filename (str): 日志文件名\n
+        log_save_folder(str): 保存logs文件夹的位置
 
     Returns:
-        logging.Logger: _description_
+        logging.Logger: 日志记录器
+
+        日志记录器的记录级别:\n
+        logger.debug('This is a customer debug message')\n
+        logger.info('This is an customer info message')\n
+        logger.warning('This is a customer warning message')\n
+        logger.error('This is an customer error message')\n
+        logger.critical('This is a customer critical message')
     """
+    # 在给定的路径下建立logs文件夹
+    log_folder = os.path.join(log_save_folder, "logs")
+    if os.path.exists(log_folder) is False:
+        os.mkdir(log_folder)
+
     log_file_path = os.path.join(log_folder, f"{filename}.log")
     logger = logging.getLogger("logger")
 
@@ -36,16 +46,5 @@ def get_logger(filename: str) -> logging.Logger:
 
     logger.addHandler(handler1)
     logger.addHandler(handler2)
-
-    # 分别为 10、30、30
-    # print(handler1.level)
-    # print(handler2.level)
-    # print(logger.level)
-
-    # logger.debug('This is a customer debug message')
-    # logger.info('This is an customer info message')
-    # logger.warning('This is a customer warning message')
-    # logger.error('This is an customer error message')
-    # logger.critical('This is a customer critical message')
 
     return logger
