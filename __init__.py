791a30747dce2a017090f2abbf84723c08fbbb82
npbcts <auditx@hotmail.com>
2023-03-27 17:02:23 +0800

    修改模块引出方式

--- __init__.py
+++ __init__.py
@@ -1,26 +1,26 @@
-from assist.datetrans import chinese2digitdate, chinese2date
-from assist.down_from_url import down_from_url
-from assist.ftpfile import FtpDownUpFile
-from assist.get_random_char import get_random_char
-from assist.iocsv import read_csv, save_dict_csv
-from assist.iojson import read_json, save_json
-from assist.iopickle import read_pickle, save_pickle
-from assist.iotext import read_text, save_text
-from assist.mylogger import get_logger
-from assist.remote_script import RemoteScript
-from assist.time_func import time_func
+# from assist.datetrans import chinese2digitdate, chinese2date
+# from assist.down_from_url import down_from_url
+# from assist.ftpfile import FtpDownUpFile
+# from assist.get_random_char import get_random_char
+# from assist.iocsv import read_csv, save_dict_csv
+# from assist.iojson import read_json, save_json
+# from assist.iopickle import read_pickle, save_pickle
+# from assist.iotext import read_text, save_text
+# from assist.mylogger import get_logger
+# from assist.remote_script import RemoteScript
+# from assist.time_func import time_func
 
 
-__all__ = [
-    "FtpDownUpFile",
-    "chinese2digitdate", "chinese2date",
-    "down_from_url",
-    "get_random_char",
-    "read_csv", "save_dict_csv",
-    "read_json", "save_json",
-    "read_pickle", "save_pickle",
-    "read_text", "save_text",
-    "get_logger",
-    "RemoteScript",
-    "time_func",
-    ]
+# __all__ = [
+#     "FtpDownUpFile",
+#     "chinese2digitdate", "chinese2date",
+#     "down_from_url",
+#     "get_random_char",
+#     "read_csv", "save_dict_csv",
+#     "read_json", "save_json",
+#     "read_pickle", "save_pickle",
+#     "read_text", "save_text",
+#     "get_logger",
+#     "RemoteScript",
+#     "time_func",
+#     ]
